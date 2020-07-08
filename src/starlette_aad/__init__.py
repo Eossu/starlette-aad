7dd7c71196133f12ae03796843313caa70de52fe
Idar Bergli <idar.bergli@protonmail.com>
2020-07-08 11:58:46 +0200

    cleanup of some code

--- src/starlette_aad/__init__.py
+++ src/starlette_aad/__init__.py
@@ -1,6 +1,12 @@
 #
 #
 #
-from importlib_metadata import version
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
-__version__ = version(__package__)
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    __version__ = "0.0.0"
--- src/starlette_aad/_middleware.py
+++ src/starlette_aad/_middleware.py
@@ -1,15 +1,11 @@
 #
 #
 #
-import base64
 import json
 import logging
 import typing
 
 import httpx
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicNumbers
 from jose import jose_jwt
 from jose.exceptions import JWTError
 from starlette.datastructures import Headers
@@ -27,43 +23,26 @@ _cache = {}
 
 
 class BaseMiddleware:
-    def __init__(
-        self, app: ASGIApp, tenant_id: str, client_id: str, discovery_endpoint: str = None,
-    ):
+    def __init__(self, app: ASGIApp, tenant_id: str, client_id: str, discovery_endpoint: str = None):
         self._app = app
         self._tenant_id = tenant_id
         self._client_id = client_id
         self._discovery_endpoint = discovery_endpoint if discovery_endpoint else BASE_URL.format(tenant_id)
-
-    def ensure_bytes(self, value):
-        if isinstance(value, str):
-            value = value.encode("utf-8")
-        return value
-
-    def decode_value(self, value):
-        decoded = base64.urlsafe_b64decode(self.ensure_bytes(value) + b"==")
-        return int.from_bytes(decoded, "big")
-
-    def rsa_pem_from_jwk(self, jwk: AzureAdKey):
-        return (
-            RSAPublicNumbers(n=self.decode_value(jwk.n), e=self.decode_value(jwk.e))
-            .public_key(default_backend())
-            .public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo,)
-        )
 
     def get_jwt(self, header: Headers) -> str:
         if "Authorization" not in header:
             return
 
         token = header.get("Authorization")
-        scheme, jwt = token.split()
-        if scheme.lower() != "bearer":
-            raise InvalidAuthorizationToken(
-                "header dose not start \
-                with 'Bearer'"
-            )
+        schema_and_token = token.split()
+        if schema_and_token[0].lower() != "bearer":
+            raise InvalidAuthorizationToken("header dose not start with 'Bearer'")
+        if len(schema_and_token) < 2:
+            raise InvalidAuthorizationToken("invalid Authorization header")
+        if len(schema_and_token) > 2:
+            raise InvalidAuthorizationToken("invalid Authorization header")
 
-        return jwt
+        return schema_and_token[1]
 
     async def get_issuer(self) -> typing.Tuple[str, str]:
         metadata = await self.get_openid_connect_metadata()
@@ -113,25 +92,19 @@ class BaseMiddleware:
                     _cache["keys"].remove(key)
                     return key
         else:
-            logger.info(
-                "Could not find the correct key, refresh OpenId \
-                Connect metadata."
-            )
+            logger.info("Could not find the correct key, refresh OpenId Connect metadata.")
             await self.get_openid_connect_metadata(force=True)
 
         return None
 
 
 class VerifyAzureAdJWT(BaseMiddleware):
-    def __init__(
-        self, app: ASGIApp, tenant_id: str, client_id: str, discovery_endpoint: str = None,
-    ):
+    def __init__(self, app: ASGIApp, tenant_id: str, client_id: str, discovery_endpoint: str = None):
         super().__init__(app, tenant_id, client_id, discovery_endpoint)
 
     async def __call__(self, scope: Scope, recive: Receive, send: Send):
         if scope["type"] not in ("http", "websocket"):
-            await self._app(scope, recive, send)
-            return
+            return await self._app(scope, recive, send)
 
         conn = HTTPConnection(scope)
         token = self.get_jwt(conn.headers)
@@ -146,19 +119,20 @@ class VerifyAzureAdJWT(BaseMiddleware):
             raise InvalidAuthorizationToken("kid missing from jwt header")
 
         key_info = await self.get_ad_key(kid)
-        public_key = self.rsa_pem_from_jwk(key_info)
         issuer = await self.get_issuer()
         audience = self._client_id
 
         try:
             decoded = jose_jwt.decode(
-                token, public_key, verify=True, algorithms=["RS256"], audience=audience, issuer=issuer,
+                token, key_info.dict(), verify=True, algorithms=["RS256"], audience=audience, issuer=issuer
             )
 
             access_token = AadAccessToken(**decoded)
             user = AadUser(access_token)
         except JWTError as e:
             raise InvalidAuthorizationToken(e.__class__.__name__)
+        except Exception as e:  # TODO: Fix this so we dont throw on everything, find out what erros we can get.
+            raise e
 
         scope["user"] = user
         await self._app(scope, recive, send)
